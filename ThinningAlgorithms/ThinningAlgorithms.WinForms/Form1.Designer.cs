306489539bb20dd2adc8a22dea8ac83106dbf717
Szymon Samuel Zborowski <zborowskis@student.mini.pw.edu.pl>
2018-11-15 19:54:03 +0100

    First build

---
+++ ThinningAlgorithms/ThinningAlgorithms.WinForms/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ThinningAlgorithms.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.modK3MBtn = new System.Windows.Forms.RadioButton();
+            this.zhangWangBtn = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.loadImageToolStripMenuItem.Text = "Load Image...";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 426);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.goBtn);
+            this.flowLayoutPanel1.Controls.Add(this.modK3MBtn);
+            this.flowLayoutPanel1.Controls.Add(this.zhangWangBtn);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 389);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(794, 34);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // goBtn
+            // 
+            this.goBtn.Location = new System.Drawing.Point(3, 3);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(75, 23);
+            this.goBtn.TabIndex = 0;
+            this.goBtn.Text = "Go!";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // modK3MBtn
+            // 
+            this.modK3MBtn.AutoSize = true;
+            this.modK3MBtn.Checked = true;
+            this.modK3MBtn.Location = new System.Drawing.Point(84, 3);
+            this.modK3MBtn.Name = "modK3MBtn";
+            this.modK3MBtn.Size = new System.Drawing.Size(123, 17);
+            this.modK3MBtn.TabIndex = 1;
+            this.modK3MBtn.TabStop = true;
+            this.modK3MBtn.Text = "Modified K3M (2016)";
+            this.modK3MBtn.UseVisualStyleBackColor = true;
+            // 
+            // zhangWangBtn
+            // 
+            this.zhangWangBtn.AutoSize = true;
+            this.zhangWangBtn.Location = new System.Drawing.Point(213, 3);
+            this.zhangWangBtn.Name = "zhangWangBtn";
+            this.zhangWangBtn.Size = new System.Drawing.Size(142, 17);
+            this.zhangWangBtn.TabIndex = 2;
+            this.zhangWangBtn.Text = "Zhang and Wang (1988)";
+            this.zhangWangBtn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(794, 380);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Thinning Algorithms";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.RadioButton modK3MBtn;
+        private System.Windows.Forms.RadioButton zhangWangBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ThinningAlgorithms/ThinningAlgorithms.WinForms/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThinningAlgorithms.WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "JPG|*.jpg|Bitmap|*.bmp|Gif|*.gif|PNG|*.png";
+            openFileDialog1.Title = "Load image";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.OpenFile());
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+            binImage();
+            if (modK3MBtn.Checked)
+            {
+                ModifiedK3M();
+            }
+            else if (zhangWangBtn.Checked)
+            {
+                ZhangAndWang();
+            }
+                
+        }
+
+        private void ModifiedK3M()
+        {
+
+        }
+
+        private void ZhangAndWang()
+        {
+
+        }
+
+        private void binImage()
+        {
+            Bitmap b = (Bitmap)pictureBox1.Image;
+            for (int i = 0; i < b.Width; i++)
+            {
+                for (int j = 0; j < b.Height; j++)
+                {
+                    if (b.GetPixel(i, j).R > 100)
+                    {
+                        b.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                    }
+                    else
+                    {
+                        b.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    }
+                }
+            }
+            pictureBox1.Image = b;
+            pictureBox1.Invalidate();
+        }
+    }
+}
