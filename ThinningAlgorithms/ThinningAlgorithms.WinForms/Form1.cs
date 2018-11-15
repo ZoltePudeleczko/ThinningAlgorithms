d5ff4ca20de4f5979260fc9b5cad3d7ac26f8177
Szymon Samuel Zborowski <zborowskis@student.mini.pw.edu.pl>
2018-11-16 00:54:58 +0100

    Zhang and Wang algorithm

--- ThinningAlgorithms/ThinningAlgorithms.WinForms/Form1.cs
+++ ThinningAlgorithms/ThinningAlgorithms.WinForms/Form1.cs
@@ -50,7 +50,110 @@ namespace ThinningAlgorithms.WinForms
 
         private void ZhangAndWang()
         {
+            System.Threading.Thread.Sleep(500);
+            Bitmap b = (Bitmap)pictureBox1.Image;
+            List<(int, int)> zwdeletable = new List<(int, int)>();
+            bool d;
+            int bp, cp;
+            do
+            {
+                zwdeletable.Clear();
+                for (int i = 0; i < b.Width; i++)
+                {
+                    for (int j = 0; j < b.Height; j++)
+                    {
+                        //1)
+                        if (b.GetPixel(i, j).ToArgb() != Color.Black.ToArgb())
+                            continue;
 
+                        //2)
+                        bp = 0;
+                        if (i + 1 < b.Width && b.GetPixel(i + 1, j).ToArgb() == Color.Black.ToArgb())
+                            bp++;
+                        if (j - 1 > 0 && i + 1 < b.Width && b.GetPixel(i + 1, j - 1).ToArgb() == Color.Black.ToArgb())
+                            bp++;
+                        if (j - 1 > 0 && b.GetPixel(i, j - 1).ToArgb() == Color.Black.ToArgb())
+                            bp++;
+                        if (i - 1 > 0 && j - 1 > 0 && b.GetPixel(i - 1, j - 1).ToArgb() == Color.Black.ToArgb())
+                            bp++;
+                        if (i - 1 > 0 && b.GetPixel(i - 1, j).ToArgb() == Color.Black.ToArgb())
+                            bp++;
+                        if (i - 1 > 0 && j + 1 < b.Height && b.GetPixel(i - 1, j + 1).ToArgb() == Color.Black.ToArgb())
+                            bp++;
+                        if (j + 1 < b.Height && b.GetPixel(i, j + 1).ToArgb() == Color.Black.ToArgb())
+                            bp++;
+                        if (i + 1 < b.Width && j + 1 < b.Height && b.GetPixel(i + 1, j + 1).ToArgb() == Color.Black.ToArgb())
+                            bp++;
+                        if (bp < 2 || bp > 6)
+                            continue;
+
+                        //3)
+                        cp = 0;
+                        if (i + 1 < b.Width && b.GetPixel(i + 1, j).ToArgb() == Color.White.ToArgb())
+                            if (j - 1 > 0 && i + 1 < b.Width && b.GetPixel(i + 1, j - 1).ToArgb() == Color.Black.ToArgb())
+                                cp++;
+                        if (j - 1 > 0 && i + 1 < b.Width && b.GetPixel(i + 1, j - 1).ToArgb() == Color.White.ToArgb())
+                            if (j - 1 > 0 && b.GetPixel(i, j - 1).ToArgb() == Color.Black.ToArgb())
+                                cp++;
+                        if (j - 1 > 0 && b.GetPixel(i, j - 1).ToArgb() == Color.White.ToArgb())
+                            if (i - 1 > 0 && j - 1 > 0 && b.GetPixel(i - 1, j - 1).ToArgb() == Color.Black.ToArgb())
+                                cp++;
+                        if (i - 1 > 0 && j - 1 > 0 && b.GetPixel(i - 1, j - 1).ToArgb() == Color.White.ToArgb())
+                            if (i - 1 > 0 && b.GetPixel(i - 1, j).ToArgb() == Color.Black.ToArgb())
+                                cp++;
+                        if (i - 1 > 0 && b.GetPixel(i - 1, j).ToArgb() == Color.White.ToArgb())
+                            if (i - 1 > 0 && j + 1 < b.Height && b.GetPixel(i - 1, j + 1).ToArgb() == Color.Black.ToArgb())
+                                cp++;
+                        if (i - 1 > 0 && j + 1 < b.Height && b.GetPixel(i - 1, j + 1).ToArgb() == Color.White.ToArgb())
+                            if (j + 1 < b.Height && b.GetPixel(i, j + 1).ToArgb() == Color.Black.ToArgb())
+                                cp++;
+                        if (j + 1 < b.Height && b.GetPixel(i, j + 1).ToArgb() == Color.White.ToArgb())
+                            if (i + 1 < b.Width && j + 1 < b.Height && b.GetPixel(i + 1, j + 1).ToArgb() == Color.Black.ToArgb())
+                                cp++;
+                        if (i + 1 < b.Width && j + 1 < b.Height && b.GetPixel(i + 1, j + 1).ToArgb() == Color.White.ToArgb())
+                            if (i + 1 < b.Width && b.GetPixel(i + 1, j).ToArgb() == Color.Black.ToArgb())
+                                cp++;
+                        if (cp != 1)
+                            continue;
+
+                        //4)
+                        d = true;
+                        if (i + 1 >= b.Width || (i + 1 < b.Width && b.GetPixel(i + 1, j).ToArgb() == Color.White.ToArgb()))
+                            if (j - 1 < 0 || (j - 1 > 0 && b.GetPixel(i, j - 1).ToArgb() == Color.White.ToArgb()))
+                                if (i - 1 < 0 || (i - 1 > 0 && b.GetPixel(i - 1, j).ToArgb() == Color.White.ToArgb()))
+                                    d = false;
+                        if (d)
+                            if (j - 2 > 0 && b.GetPixel(i, j - 2).ToArgb() == Color.Black.ToArgb())
+                                d = false;
+                        if (d)
+                            continue;
+
+                        //5)
+                        d = true;
+                        if (i + 1 >= b.Width || (i + 1 < b.Width && b.GetPixel(i + 1, j).ToArgb() == Color.White.ToArgb()))
+                            if (j - 1 < 0 || (j - 1 > 0 && b.GetPixel(i, j - 1).ToArgb() == Color.White.ToArgb()))
+                                if (i + 1 >= b.Width || (i + 1 < b.Width && b.GetPixel(i + 1, j).ToArgb() == Color.White.ToArgb()))
+                                    d = false;
+                        if (d)
+                            if (i + 2 < b.Width && b.GetPixel(i + 2, j).ToArgb() == Color.Black.ToArgb())
+                                d = false;
+                        if (d)
+                            continue;
+
+                        zwdeletable.Add((i, j));
+                    }
+                }
+                foreach ((int, int) p in zwdeletable)
+                {
+                    b.SetPixel(p.Item1, p.Item2, Color.White);
+                }
+
+                pictureBox1.Image = b;
+                pictureBox1.Refresh();
+                System.Threading.Thread.Sleep(500);
+            } while (zwdeletable.Count != 0);
+            pictureBox1.Image = b;
+            pictureBox1.Refresh();
         }
 
         private void binImage()
@@ -60,18 +163,26 @@ namespace ThinningAlgorithms.WinForms
             {
                 for (int j = 0; j < b.Height; j++)
                 {
-                    if (b.GetPixel(i, j).R > 100)
+                    int n = (b.GetPixel(i, j).R + b.GetPixel(i, j).G + b.GetPixel(i, j).B) / 3;
+                    b.SetPixel(i, j, Color.FromArgb(n, n, n));
+                }
+            }
+            for (int i = 0; i < b.Width; i++)
+            {
+                for (int j = 0; j < b.Height; j++)
+                {
+                    if (b.GetPixel(i, j).R < 100)
                     {
-                        b.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                        b.SetPixel(i, j, Color.Black);
                     }
                     else
                     {
-                        b.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                        b.SetPixel(i, j, Color.White);
                     }
                 }
             }
             pictureBox1.Image = b;
-            pictureBox1.Invalidate();
+            pictureBox1.Refresh();
         }
     }
 }
